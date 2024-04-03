74529714c8308865bfb0f0b5ea5871402990c401
junohpark221 <junohpark221@gmail.com>
2024-04-03 16:01:24 +0900

    refactor: 순위 확인 기능 구현 위치 수정 및 Ranking enum 일부 수정

--- src/main/java/lotto/enums/Ranking.java
+++ src/main/java/lotto/enums/Ranking.java
@@ -1,10 +1,12 @@
 package lotto.enums;
 
+import java.util.Arrays;
+
 public enum Ranking {
     FAIL(0, 0),
-    FIFTH(5000, 2),
-    FOURTH(50000, 3),
-    THIRD(1500000, 4),
+    FIFTH(5000, 3),
+    FOURTH(50000, 4),
+    THIRD(1500000, 5),
     SECOND(30000000, 5),
     FIRST(2000000000, 6);
 
@@ -22,5 +24,20 @@ public enum Ranking {
 
     public int getMatchCount() {
         return matchCount;
+    }
+
+    public static Ranking getRank(long answerCount, boolean isCorrectBonusBall) {
+        if (answerCount == 5 && isCorrectBonusBall) {
+            return SECOND;
+        }
+
+        if (answerCount < FIFTH.matchCount) {
+            return FAIL;
+        }
+
+        return Arrays.stream(values())
+                .filter(rank -> rank.matchCount == answerCount)
+                .findFirst()
+                .orElse(null);
     }
 }
