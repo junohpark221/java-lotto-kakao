74e61a002fb84f51282d383cc626fd410f3a2dc7
junohpark221 <junohpark221@gmail.com>
2024-04-04 15:16:29 +0900

    refactor: 테스트 코드 리팩토링

--- src/test/java/lotto/domain/BallTest.java
+++ src/test/java/lotto/domain/BallTest.java
@@ -1,6 +1,7 @@
 package lotto.domain;
 
 import lotto.domain.Ball;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,8 +17,17 @@ public class BallTest {
 
     @Test
     void 볼_번호_범위_테스트() {
-        assertThatThrownBy(() -> {
-            new Ball(46);
-        }).isInstanceOf(RuntimeException.class);
+        Assertions.assertAll(() -> {
+            assertThatThrownBy(() -> {
+                new Ball(0);
+            }).isInstanceOf(IllegalArgumentException.class);
+            Ball ball01 = new Ball(1);
+            assertThat(ball01.getNumber()).isEqualTo(1);
+            Ball ball45 = new Ball(45);
+            assertThat(ball45.getNumber()).isEqualTo(45);
+            assertThatThrownBy(() -> {
+                new Ball(46);
+            }).isInstanceOf(IllegalArgumentException.class);
+        });
     }
 }
