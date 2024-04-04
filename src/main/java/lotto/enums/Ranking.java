37227767ac238641e21f6e64b801ef4f66f271f8
junohpark221 <junohpark221@gmail.com>
2024-04-04 11:42:09 +0900

    refactor: enum내 람다식 사용을 통한 당첨 여부 판단 개선

--- src/main/java/lotto/enums/Ranking.java
+++ src/main/java/lotto/enums/Ranking.java
@@ -1,21 +1,24 @@
 package lotto.enums;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 
 public enum Ranking {
-    FAIL(0, 0),
-    FIFTH(5000, 3),
-    FOURTH(50000, 4),
-    THIRD(1500000, 5),
-    SECOND(30000000, 5),
-    FIRST(2000000000, 6);
+    FAIL(0, 0, (answerCount, isCorrectBonusBall) -> answerCount < 2),
+    FIFTH(5000, 3, (answerCount, isCorrectBonusBall) -> answerCount == 3),
+    FOURTH(50000, 4, (answerCount, isCorrectBonusBall) -> answerCount == 4),
+    THIRD(1500000, 5, (answerCount, isCorrectBonusBall) -> answerCount == 5 && !isCorrectBonusBall),
+    SECOND(30000000, 5, (answerCount, isCorrectBonusBall) -> answerCount == 5 && isCorrectBonusBall),
+    FIRST(2000000000, 6, (answerCount, isCorrectBonusBall) -> answerCount == 6);
 
     private final long reward;
     private final int matchCount;
+    private final BiFunction<Long, Boolean, Boolean> checkMatch;
 
-    Ranking(long reward, int matchCount) {
+    Ranking(long reward, int matchCount, BiFunction<Long, Boolean, Boolean> checkMatch) {
         this.reward = reward;
         this.matchCount = matchCount;
+        this.checkMatch = checkMatch;
     }
 
     public long getReward() {
@@ -26,19 +29,10 @@ public enum Ranking {
         return matchCount;
     }
 
-
     public static Ranking getRank(long answerCount, boolean isCorrectBonusBall) {
-        if (answerCount == 5 && isCorrectBonusBall) {
-            return SECOND;
-        }
-
-        if (answerCount < FIFTH.matchCount) {
-            return FAIL;
-        }
-
         return Arrays.stream(values())
-                .filter(rank -> rank.matchCount == answerCount)
+                .filter(ranking -> ranking.checkMatch.apply(answerCount, isCorrectBonusBall))
                 .findFirst()
-                .orElse(null);
+                .orElse(FAIL);
     }
 }
